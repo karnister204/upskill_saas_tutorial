0bf3447e6c392f48000cb090267f3695fc39caf2
Popar Fapos <POPARfaposek@gmail.com>
2016-12-22 13:02:02 +0000

    Changes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,10 +7,14 @@ class ContactsController < ApplicationController
    def create
       @contact = Contact.new(contact_params)
       if @contact.save
-         flash[:succes] = "Message sent."
+         name = params[:contact][:name]
+         email = params[:contact][:email]
+         body = params[:contact][:comments]
+         ContactMailer.contact_email(name, email, body).deliver
+         flash[:success] = "Message sent."
          redirect_to new_contact_path
       else
-         flash[:error] = @contact.errors.full_messages.join(", ")
+         flash[:danger] = @contact.errors.full_messages.join(", ")
          redirect_to new_contact_path
       end
    end
